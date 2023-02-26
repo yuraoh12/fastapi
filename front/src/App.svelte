044f1b75b37fd14cac67cf3a3f86f64b54a86a05
OH YURA <102283560+yuraoh12@users.noreply.github.com>
2023-02-26 10:48:12 +0900

    Add files via upload

---
+++ front/src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+	//let message;
+
+	/*
+	fetch("http://127.0.0.1:8000/hello").then((res) => {
+		res.json().then((json) => {
+			message = json.message;
+		})
+	})
+	*/
+	async function hello() {
+		const res = await fetch("http://127.0.0.1:8000/hello");
+		const json = await res.json();
+
+		if (res.ok) {
+			return json.message;
+		} else {
+			alert("error");
+		}
+	}
+
+	let promise = hello();
+</script>
+
+{#await promise}
+	<p>... waiting</p>
+{:then message} 
+	<h1>{message}</h1>
+{/await}
+<h1>hihi</h1>
